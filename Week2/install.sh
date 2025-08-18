a7b2db0756eb05afb131ef3e9415557d7b2e76dc
Diego Solorzano <diego.solorzano@nxp.com>
2025-08-18 10:17:29 -0600

    [Week2] Adding week 2 files

--- Week2/install.sh
+++ Week2/install.sh
@@ -1,4 +1,4 @@
-pip install --break-system-packages -r requirements.txt
+pip install --break-system-packages -r ./requirements.txt
 apt-get update
 apt-get install  libgl1
 python3 -m spacy download en_core_web_sm --break-system-packages
